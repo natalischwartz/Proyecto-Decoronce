1a51a54f494946d530299467f94463e133f199de
natali sch <schwartznatali@gmail.com>
2022-12-26 14:30:41 -0300

    agregue navbar, iframe y boton scrollTop

---
+++ main.js
@@ -0,0 +1,25 @@
+//vinculado
+
+console.log("aqui estoy ")
+
+//BOTON SUBIR ARRIBA 
+
+document.getElementById("topBtn").addEventListener("click", function(){
+    document.documentElement.scrollTop=0;
+}
+);
+
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ){
+        document.getElementById("topBtn").style.display= "block";
+    }
+    else{
+        document.getElementById("topBtn").style.display="none";
+    }   
+}
+
