43e0bcfeb7f2aed40222bc4117830db55b3ccb76
natali sch <schwartznatali@gmail.com>
2023-01-12 01:11:24 -0300

    despliega el navbar, cambie fotos, seccion categorias modifique

--- js/main.js
+++ js/main.js
@@ -28,5 +28,42 @@ function scrollFunction() {
 
 
 
-//BOTONES CATEGORIAS
 
+
+//VENTANA MODAL-blackout
+
+let cerrar = document.querySelectorAll(".close")[0];
+let abrir = document.querySelectorAll(".cta")[0];
+let modal = document.querySelectorAll(".modall")[0];
+let modalC = document.querySelectorAll(".modal-container")[0];
+
+
+abrir.addEventListener("click", function (e){
+    e.preventDefault();
+    modalC.style.opacity = "1";
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+});
+
+cerrar.addEventListener("click", function(){
+    modal.classList.toggle("modal-close");
+    
+    setTimeout(function(){
+        modalC.style.opacity = "0";
+        modalC.style.visibility = "hidden";
+    },850);
+
+});
+
+window.addEventListener("click", function(e){
+    console.log(e.target);
+    if(e.target == modalC){
+        modal.classList.toggle("modal-close");
+    
+        setTimeout(function(){
+            modalC.style.opacity = "0";
+            modalC.style.visibility = "hidden";
+        },850);
+    
+    };
+});
